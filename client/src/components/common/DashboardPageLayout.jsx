9d610e254d4fd65af80ab7c9f3bec05a1a563395
AbhishekNavgan95 <navganabhishek90@gmail.com>
2024-07-31 22:56:26 +0530

    style : dashboard layout changes

--- client/src/components/common/DashboardPageLayout.jsx
+++ client/src/components/common/DashboardPageLayout.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const DashboardPageLayout = ({ children }) => {
     return (
         <div
-            className='text-night-900 md:ml-52 md:mr-2 mt-[5rem] mb-5 w-full'
+            className='text-night-900 md:ml-48 mt-[4rem] mb-3 w-full'
         >
-            <div className='rounded-lg px-5 md:m-10 min-h-[calc(100vh-6rem)] md:min-h-[calc(100vh-10rem)] flex items-start'>
+            <div className='rounded-lg bg-night-25 border border-night-100 px-5 md:m-10 min-h-[calc(100vh-5rem)] md:min-h-[calc(100vh-7rem)] flex items-start'>
                 {children}
             </div>
         </div>
